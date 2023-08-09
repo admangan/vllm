6600c180c0c10f563d17f52f64b7dd84f2436458
Andrew Mangan <admangan2018@gmail.com>
2023-08-09 11:20:27 -0400

    trying again

--- vllm/entrypoints/api_server.py
+++ vllm/entrypoints/api_server.py
@@ -16,66 +16,64 @@ from vllm.utils import random_uuid
 TIMEOUT_KEEP_ALIVE = 5  # seconds.
 TIMEOUT_TO_PREVENT_DEADLOCK = 1  # seconds.
 app = FastAPI()
+    
 
-
-@app.post("/generate")
-async def generate(request: Request) -> Response:
-    """Generate completion for the request.
-
-    The request should be a JSON object with the following fields:
-    - prompt: the prompt to use for the generation.
-    - stream: whether to stream the results or not.
-    - other fields: the sampling parameters (See `SamplingParams` for details).
-    """
-    request_dict = await request.json()
-    prompt = request_dict.pop("prompt")
-    stream = request_dict.pop("stream", False)
-    sampling_params = SamplingParams(**request_dict)
-    request_id = random_uuid()
-    results_generator = engine.generate(prompt, sampling_params, request_id)
-
-    # Streaming case
-    async def stream_results() -> AsyncGenerator[bytes, None]:
-        async for request_output in results_generator:
-            prompt = request_output.prompt
-            text_outputs = [
-                prompt + output.text for output in request_output.outputs
-            ]
-            ret = {"text": text_outputs}
-            yield (json.dumps(ret) + "\0").encode("utf-8")
-
-    async def abort_request() -> None:
-        await engine.abort(request_id)
-
-    if stream:
-        background_tasks = BackgroundTasks()
-        # Abort the request if the client disconnects.
-        background_tasks.add_task(abort_request)
-        return StreamingResponse(stream_results(), background=background_tasks)
-
-    # Non-streaming case
-    final_output = None
-    async for request_output in results_generator:
-        if await request.is_disconnected():
-            # Abort the request if the client disconnects.
-            await engine.abort(request_id)
-            return Response(status_code=499)
-        final_output = request_output
-
-    assert final_output is not None
-    prompt = final_output.prompt
-    text_outputs = [prompt + output.text for output in final_output.outputs]
-    ret = {"text": text_outputs}
-    return JSONResponse(ret)
-
-
+@serve.deployment(num_replicas=4, ray_actor_options={"num_gpus": 1}, route_prefix="/")
+@serve.ingress(app)
 class LanguageModel:
     def __init__(self):
         self.engine_args = AsyncEngineArgs.from_cli_args(args)
         self.engine = AsyncLLMEngine.from_engine_args(self.engine_args)
 
+    @app.post("/generate")
     async def generate(self, request):
-        return await app(request)
+        """Generate completion for the request.
+
+        The request should be a JSON object with the following fields:
+        - prompt: the prompt to use for the generation.
+        - stream: whether to stream the results or not.
+        - other fields: the sampling parameters (See `SamplingParams` for details).
+        """
+        request_dict = await request.json()
+        prompt = request_dict.pop("prompt")
+        stream = request_dict.pop("stream", False)
+        sampling_params = SamplingParams(**request_dict)
+        request_id = random_uuid()
+        results_generator = self.engine.generate(prompt, sampling_params, request_id)
+
+        # Streaming case
+        async def stream_results() -> AsyncGenerator[bytes, None]:
+            async for request_output in results_generator:
+                prompt = request_output.prompt
+                text_outputs = [
+                    prompt + output.text for output in request_output.outputs
+                ]
+                ret = {"text": text_outputs}
+                yield (json.dumps(ret) + "\0").encode("utf-8")
+
+        async def abort_request() -> None:
+            await self.engine.abort(request_id)
+
+        if stream:
+            background_tasks = BackgroundTasks()
+            # Abort the request if the client disconnects.
+            background_tasks.add_task(abort_request)
+            return StreamingResponse(stream_results(), background=background_tasks)
+
+        # Non-streaming case
+        final_output = None
+        async for request_output in results_generator:
+            if await request.is_disconnected():
+                # Abort the request if the client disconnects.
+                await self.engine.abort(request_id)
+                return Response(status_code=499)
+            final_output = request_output
+
+        assert final_output is not None
+        prompt = final_output.prompt
+        text_outputs = [prompt + output.text for output in final_output.outputs]
+        ret = {"text": text_outputs}
+        return JSONResponse(ret)
 
 
 if __name__ == "__main__":
@@ -85,23 +83,16 @@ if __name__ == "__main__":
     parser = AsyncEngineArgs.add_cli_args(parser)
     args = parser.parse_args()
 
-    if not ray.is_initialized():
-        ray.init()
-
+    
+    languageModel = LanguageModel()
     # Create a Ray Serve instance.
-    client = serve.start()
-
-    #define the backend with the needed number of replicas and GPU requirements
-    client.create_backend("llm_backend", LanguageModel, num_replicas=4, resources={"GPU": 1})
-
-    #link the backend to an endpoint
-    client.create_endpoint("llm_endpoint", backend="llm_backend", route="/generate", methods=["POST"])
+    
 
     print("Serving on http://{}:{}".format(args.host, args.port))
 
     # engine_args = AsyncEngineArgs.from_cli_args(args)
     # engine = AsyncLLMEngine.from_engine_args(engine_args)
-    
+
     # uvicorn.run(app,
     #             host=args.host,
     #             port=args.port,
